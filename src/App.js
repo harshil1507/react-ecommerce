fac97f8340880492c056edb3f456dc8e3f1d7e77
Harshil Maniar <maniarharshil1000@gmail.com>
2021-05-11 16:03:55 +0530

    components for homepage and styling added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomePage from "./pages/homepage/homepage.component";
+import './app.css';
+
+function App() {
+    return (
+        <div className="App">
+            <HomePage />
+        </div>
+    );
+}
+export default App;
